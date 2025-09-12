8ccbdaecfc8b7cf4029758d932ffc03fbebd1ba9
Всеволод Гоголев <seva-gogolev@mail.ru>
2025-09-12 14:41:43 +0300

    Task3

---
+++ Tyuiu.GogolevVm.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.GogolevVm.Sprint0.Task3.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
